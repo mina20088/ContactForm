b4de2a8244e0472ff755e78d348e9b7b35e5ae7b
mina20088 <minaremon1986@gmail.com>
2020-09-12 23:33:14 +0200

    @ SANITIZE.php added the function SanitizeString to make sanitization

--- Global Functions/SANITIZE.php
+++ Global Functions/SANITIZE.php
@@ -1,1 +1,26 @@
 <?php
+function SanitizeString($String,$Sanitize_filters_ID,$Flags){
+    $FilteredString = "";
+    if(isset($String)){
+        if(is_array($String)){
+            if(is_array($Sanitize_filters_ID)){
+                if($Flags!= null){
+                    for($i = 0; $i<count($String); $i++){
+                        for($k = 0; $k<count($Sanitize_filters_ID); $k++){
+                            for($n = 0; $n<count($Flags); $n++){
+                                $FilteredString = filter_var($String[$i],$Sanitize_filters_ID[$k],$Flags[$n]);
+                            }
+                        }
+                    }
+                }else{
+                    for($i = 0; $i<count($String); $i++) {
+                        for ($k = 0; $k < count($Sanitize_filters_ID); $k++) {
+                            $FilteredString = filter_var($String[$i],$Sanitize_filters_ID[$k]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return $FilteredString ;
+}
