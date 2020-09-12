1e16d961e7f16b14bcbc2d3432ac544a6c60de65
mina20088 <minaremon1986@gmail.com>
2020-09-12 23:32:12 +0200

    @ ContactMeProcess.php added include SANITIZE.php to sanitize string

--- Process/ContactMeProcess.php
+++ Process/ContactMeProcess.php
@@ -1,9 +1,8 @@
 <?php
+include "../Global Functions/SANITIZE.php";
 if(isset($_POST['Submit'])){
-    $FullName = $_POST['FullName'];
-    $Email = $_POST['Email'];
+    $FullName = SanitizeString([$_POST['FullName']],[FILTER_SANITIZE_SPECIAL_CHARS,FILTER_SANITIZE_STRING],null);
+    $Email = SanitizeString([$_POST['Email']],[FILTER_SANITIZE_EMAIL],null);
     $Telephone = $_POST['phone-full'];
-    $Message = $_POST['message'];
-
-    var_dump($_POST);
+    $Message = SanitizeString([$_POST['message']],[FILTER_SANITIZE_SPECIAL_CHARS,FILTER_SANITIZE_STRING],null);
 }
