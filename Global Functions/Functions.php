4ea5cc9e05a5c882770dd99422bcbbb112159341
mina20088 <minaremon1986@gmail.com>
2020-09-15 14:07:02 +0200

    removed the Functions.php

--- Global Functions/Functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getError($String1, $String2){
-    if(isset($String)){
-        echo "<span style='color: red'>$String1</span>";
-    }elseif (isset($String2)){
-        echo "<span style='color: red'>$String2</span>";
-    }
-}
