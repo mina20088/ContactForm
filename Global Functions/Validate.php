2a7b35e7d4007a53d9e34a33f494e7c6ddbe7784
mina20088 <minaremon1986@gmail.com>
2020-09-15 19:05:28 +0200

    @ Validate.php addw the function 1-ValidateEmail: to validate Email 2-ValidatePhoneNumber : to validate Phone Number 3-ValidatePhoneNumberLenght : to validate the lenght of the phone number @ ContactMeProcess.php adde         if(!ValidateString($FullName)) {             $QueryString .= "Error1=please use string characters only in first name&"."firstname="."".'&email='.$Email.'&telephone='.$Telephone.'&message='.$Message;         }          if(filter_var($Email,FILTER_VALIDATE_EMAIL)){              if(!ValidateEmail($Email)){                  $QueryString .= "Error2=Please Enter A Valid Email"."&firstname=".$FullName."&email=".$Email."&telephone=".$Telephone."&message=".$Message;              }         if(!ValidatePhoneNumber($Telephone)){             $QueryString .= "Error3=Please Enter A valid Phone Number,Numbers Only Allowed"."&firstname=".$FullName."&email=".$Email."&message=".$Message;             if(!ValidatePhoneNumberLenght($Telephone)){                 $QueryString .= "Error5=phone Number Length Allowed Between [1-15] Digit"."&firstname=".$FullName."&email=".$Email."&message=".$Message;             }

--- Global Functions/Validate.php
+++ Global Functions/Validate.php
@@ -1,5 +1,5 @@
 <?php
-function CheckStringValue($String){
+function ValidateString($String){
     $Status = false;
     if(preg_match_all("/^([a-zA-Z\s]*(\s)*)$/i",$String,$matches)){
         $Status = true;
@@ -8,5 +8,32 @@ function CheckStringValue($String){
     }
     return $Status;
 }
-
-
+/*validate the emil address according to the RFCC5322 email standard*/
+function ValidateEmail($Email){
+    $Status = false;
+    if(preg_match_all("/[a-z0-9!#$%&'*+\/=?^_‘{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_‘{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/",$Email,$matches)){
+        $Status = true;
+    }else{
+        $Status = false;
+    }
+    return $Status;
+}
+function ValidatePhoneNumber($phoneNumber){
+    $Status = false;
+    if(filter_var($phoneNumber,FILTER_VALIDATE_INT)){
+        $Status = true;
+    }else{
+        $Status = false;
+    }
+    return $Status;
+}
+function ValidatePhoneNumberLenght($PhoneNumber){
+    $Status = false;
+    $phoneNumberLength = strlen($PhoneNumber);
+    if($phoneNumberLength > 0 && $phoneNumberLength < 15){
+        $Status = true;
+    }else{
+        $Status = false;
+    }
+    return $Status;
+}
